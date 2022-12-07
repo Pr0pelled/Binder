d5cc25b841154a79221d0d6f3377b1f50ff8556e
Pr0pelled <pr0pelled.12@gmail.com>
2022-12-06 21:47:29 -0500

    Initial Commit

---
+++ src/init.lua
@@ -0,0 +1,39 @@
+--[[
+	A custom implementation of a binder using CollectionService
+	Written by Pr0pelled
+	12/06/2022
+]]
+
+--[[ Types ]]
+type func = (any?) -> (any?)
+
+type module = {
+    [string]: func?
+}
+
+--[[ Services ]]
+
+
+--[[ Dependencies ]]
+
+
+--[[ Variables ]]
+
+--[=[
+	@class Binder
+	A custom implementation of a binder using CollectionService
+]=]
+local Binder: module = {}
+
+--[[ Functions ]]
+
+--[[ Functions ]]
+
+--[ Helper Functions ]
+
+--[ Constructors ]
+
+--[ Methods ]
+
+--Returning Binder
+return Binder
