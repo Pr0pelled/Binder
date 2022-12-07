0e43c54da93bff099c00ae880788f1da6318b6f0
Pr0pelled <pr0pelled.12@gmail.com>
2022-12-06 23:05:45 -0500

    Added NEW constructor

--- src/init.lua
+++ src/init.lua
@@ -21,17 +21,28 @@ type module = {
 
 --[=[
 	@class Binder
-	A custom implementation of a binder using CollectionService
+	A custom implementation binder using CollectionService
 ]=]
 local Binder: module = {}
 
 --[[ Functions ]]
 
---[[ Functions ]]
-
---[ Helper Functions ]
+--[ Helpers ]
 
 --[ Constructors ]
+
+--[=[
+	Constructs a new `binder`
+
+	@within Binder
+	@param tagName string -- The name of the `CollectionService` tag to be used
+	@param constructor function --The function to run when a new instance containing the tag is created
+	@param ... any? --Arguments to be passed to the constructor
+	@return binder
+]=]
+function Binder.new(tagName: string, constructor: func, ...: any?)
+
+end
 
 --[ Methods ]
 
