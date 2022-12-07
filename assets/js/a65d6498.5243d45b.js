0754e9fbc173f55a95ab447b7a2035ec847d7bcc
Pr0pelled <pr0pelled.12@gmail.com>
2022-12-06 23:03:41 -0500

    Built and published by Moonwave

--- assets/js/a65d6498.5243d45b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[755],{28580:e=>{e.exports=JSON.parse('{"functions":[],"properties":[],"types":[],"name":"Binder","desc":"A custom implementation of a binder using CollectionService","source":{"line":26,"path":"src/init.lua"}}')}}]);
